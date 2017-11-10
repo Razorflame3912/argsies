32f6370972556dc45f06e3598737f24f8e1e2c29
Razorflame3912 <melsharawy@stuy.edu>
2017-11-10 13:06:08 -0500

    slightly more fix

--- parse.c
+++ parse.c
@@ -12,15 +12,16 @@ char ** parse_args (char * line){
   //printf("%s\n",line);
   char line2[200];
   char * line3 = strcpy(line2, line);
-  char ** arr = malloc(6 * sizeof(char*));
+  char ** arr = (char**)malloc(6 * sizeof(char*));
   int i = 0;
   while(line3){
     //    printf("%s\n",line2);
     arr[i] = strsep(&line3," ");
-    printf("%s\n",line3);
+    printf("arr[%d]:\t[%s]\n",i,arr[i]);
     i++;
   }
   arr[i] = NULL;
+  printf("arr[%d]:\t[%s]\n",i,arr[i]);
   return arr;
 }
 
@@ -30,7 +31,16 @@ int main(){
   printf("[%s]\n", strsep( &s1, "z" ));
   printf("[%s]\n", s1);*/
   char * s = "ls -a -l";
+  //printf("1\n");
   char ** args = parse_args(s);
+  //printf("2\n");
+  int j = 0;
+  //printf("3\n");
+  printf("args[%d]:\t[%s]\n",0,args[0]);
+  printf("args[%d]:\t[%s]\n",1,args[1]);
+  printf("args[%d]:\t[%s]\n",2,args[2]);
+  printf("args[%d]:\t[%s]\n",3,args[3]);
+
   execvp(args[0],args);
   
   return 0;
